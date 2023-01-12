c60c2db472268a1275814c31318377e17d3cce64
vddCore <vddcore@outlook.com>
2023-01-12 11:34:12 +0100

    Added statistics display control.

---
+++ OverCR.StatX/Controls/StatisticsDisplayLeftSide.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+
+namespace OverCR.StatX.Controls
+{
+    public partial class StatisticsDisplayLeftSide
+    {
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set
+            {
+                SetValue(TitleProperty, value);
+            }
+        }
+
+        public string UnitName
+        {
+            get { return (string)GetValue(UnitNameProperty); }
+            set
+            {
+                SetValue(UnitNameProperty, value);
+            }
+        }
+
+        public float Value
+        {
+            get { return (float)GetValue(ValueProperty); }
+            set
+            {
+                SetValue(ValueProperty, value);
+            }
+        }
+
+        public static readonly DependencyProperty TitleProperty = DependencyProperty.Register("Title", typeof(string), typeof(StatisticsDisplayLeftSide));
+        public static readonly DependencyProperty UnitNameProperty = DependencyProperty.Register("UnitName", typeof(string), typeof(StatisticsDisplayLeftSide));
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register("Value", typeof(float), typeof(StatisticsDisplayLeftSide));
+
+        public StatisticsDisplayLeftSide()
+        {
+            InitializeComponent();
+        }
+    }
+}
